837a540a4e8522217099a865801fee4ff68f66fa
Ryan Brennan <95979871+ZeroTolerance225@users.noreply.github.com>
2024-01-28 13:56:50 -0500

    Update Read-and-Filter-BlackOutBoarder.py

--- PlateReading/Read-and-Filter-BlackOutBoarder.py
+++ PlateReading/Read-and-Filter-BlackOutBoarder.py
@@ -1,12 +1,11 @@
-listofstates = [
-    "ALABAMA", "ALASKA", "ARIZONA", "ARKANSAS", "CALIFORNIA",
-    "COLORADO", "CONNECTICUT", "DELAWARE", "FLORIDA", "GEORGIA",
-    "HAWAII", "IDAHO", "ILLINOIS", "INDIANA", "IOWA",
-    "KANSAS", "KENTUCKY", "LOUISIANA", "MAINE", "MARYLAND",
-    "MASSACHUSETTS", "MICHIGAN", "MINNESOTA", "MISSISSIPPI", "MISSOURI",
-    "MONTANA", "NEBRASKA", "NEVADA", "NEW HAMPSHIRE", "NEW JERSEY",
-    "NEW MEXICO", "NEW YORK", "NORTH CAROLINA", "NORTH DAKOTA", "OHIO",
-    "OKLAHOMA", "OREGON", "PENNSYLVANIA", "RHODE ISLAND", "SOUTH CAROLINA",
-    "SOUTH DAKOTA", "TENNESSEE", "TEXAS", "UTAH", "VERMONT",
-    "VIRGINIA", "WASHINGTON", "WEST VIRGINIA", "WISCONSIN", "WYOMING"
-]
+'''
+Description:
+This code uses EasyOCR and OpenCV to make the bounding boxes and pull the text from the plate.
+Once the bounding boxes are made the largest bounding box is identified and the remaining image 
+outside of the boxes is blacked ouyt and deemed unecessary. Since the plate number will always be 
+the largest boundng box on the plate the text from this box is used to obtain the plate number. Then 
+all of the text found is stored in a string which we eventually use to find the state.
+
+Potential Issues:
+If there are errors in pulling the exact state name then the state name will not be found from the list of 50.
+'''
