a6e02500565e90d7a515fb13f30db1cdcaa40e3e
Ryan Brennan <95979871+ZeroTolerance225@users.noreply.github.com>
2024-01-28 13:47:19 -0500

    Update Read-and-Filtering.py

--- PlateReading/Read-and-Filtering.py
+++ PlateReading/Read-and-Filtering.py
@@ -1,3 +1,15 @@
+'''
+Description: 
+This code uses EasyOCR and OpenCV to make the bounding boxes and pull the sext from the plate. 
+Once the bounding boxes are made the largest bounding box is identified and then the color is turned red. 
+Since the plate number will always be the largest boundng box on the plate the text from this box is used to 
+obtain the plate number. Then all of the text found is stored in a string which we eventually use to find 
+the state from.
+
+Potential Issues:
+If there are errors in pulling the exact state name then the state name will not be found from the list of 50.
+'''
+
 import cv2
 import easyocr
 
