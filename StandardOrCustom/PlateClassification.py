5e8f2c80041cbd0178a5f718d883650904c3a154
Ryan Brennan <95979871+ZeroTolerance225@users.noreply.github.com>
2024-02-09 17:41:33 -0500

    Update and rename PlateClassification.py to StandardOrCustom/PlateClassification.py

--- StandardOrCustom/PlateClassification.py
+++ StandardOrCustom/PlateClassification.py
@@ -1,13 +1,6 @@
 '''
-Description:
-This code uses EasyOCR and OpenCV to make the bounding boxes and pull the text from the plate.
-Once the bounding boxes are made the largest bounding box is identified and then the color is turned red.
-Since the plate number will always be the largest boundng box on the plate the text from this box is used to
-obtain the plate number. Then all of the text found is stored in a string which we eventually use to find
-the state from.
-
-Potential Issues:
-If there are errors in pulling the exact state name then the state name will not be found from the list of 50.
+Description: Uses regex to check if the plate number is standard or custom.
+Looks at the list of all possible US plate standard combinations
 '''
 
 
