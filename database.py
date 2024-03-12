dbbba4c28fe6b5808255b96589d54d983c169b1c
JeffreyTet <128191757+JeffreyTet@users.noreply.github.com>
2024-03-12 13:32:06 -0400

    Update database.py
    modified the functions to impliment a dated registration system that would delete expired registers and added delete and extension functions.

--- database.py
+++ database.py
@@ -18,47 +18,38 @@ engine = create_engine(
 
 def load_all_registered_from_db():
     with engine.connect() as conn:
-        result_Register = conn.execute(text("select * from Registration"))
-        result_all_Register = result_Register.all()
+        search_Result = conn.execute(text("select * from Registration where EndReg <= CURRENT_DATE;"))
+        findings = search_Result.all()
+        if len(findings) == 0:
+            result_Register = conn.execute(text("select * from Registration"))
+            result_all_Register = result_Register.all()
 
-        Registery = []
+            Registry = []
 
-        for rows in result_all_Register:
-            Registery.append(rows)
-        conn.commit()
+            for rows in result_all_Register:
+                Registry.append(rows)
+            conn.commit()
 
-        return Registery
+            return Registry
+        else:
+            conn.execute(text("delete from Registration where EndReg <= CURRENT_DATE;"))
+            result_Register = conn.execute(text("select * from Registration"))
+            result_all_Register = result_Register.all()
+
+            Registry = []
+
+            for rows in result_all_Register:
+                Registry.append(rows)
+            conn.commit()
+
+            return Registry
 
 
-def load_vehicles_from_db():
+
+def load_registered_from_db(PlateNum, PlateState):
     with engine.connect() as conn:
-        result_Vehicle = conn.execute(text("select * from Vehicle"))
-        result_all_Vehicle = result_Vehicle.all()
-
-        Vehicles = []
-
-        for rows in result_all_Vehicle:
-            Vehicles.append(rows)
-
-        return Vehicles
-
-
-def load_all_unregistered_from_db():
-    with engine.connect() as conn:
-        result_Unregistered = conn.execute(text("select * from Unregistered"))
-        result_all_Unregistered = result_Unregistered.all()
-
-        Unregistered = []
-
-        for rows in result_all_Unregistered:
-            Unregistered.append(rows)
-
-        return Unregistered
-
-
-def load_registered_from_db(PlateNum):
-    with engine.connect() as conn:
-        result_Register = conn.execute(text("select * from Registration where PlateNum = :val", val=PlateNum))
+        result_Register = conn.execute(
+            text("select * from Registration where PlateNum = :val and PlateState = :val2", val=PlateNum, val2=PlateState))
 
         findings = result_Register.all()
         if len(findings) == 0:
@@ -69,14 +60,66 @@ def load_registered_from_db(PlateNum):
             return findings
 
 
+
 def add_register_to_db(data):
     with engine.connect() as conn:
         params = {
             'ID': data['ID#'],
             'plate': data['Plate#'],
             'state': data['State'],
-            'email': data['Email']
-         }
-        conn.execute(text("insert into Registration values (:ID, :plate, :state, :email);"), params)
+            'email': data['Email'],
+            'make': data['Make'],
+            'model': data['Model'],
+            'name': data['Name'],
+            'personType': data['PersonType']
+        }
+        result_Register = conn.execute(text("select * from Registration where PlateNum = :plate and PlateState = :state"), params)
+        findings = result_Register.all()
+        if len(findings) == 0:
+            if params['personType'] == 'Visitor':
+                conn.execute(text("insert into Registration values (:ID,:name, :plate, :state,:make,:model,:personType, :email, Current_TIMESTAMP, Current_DATE+1);"), params)
+                conn.commit()
+            else:
+                conn.execute(text("insert into Registration values (:ID,:name, :plate, :state,:make,:model,:personType, :email, Current_TIMESTAMP,DATE_ADD(CURRENT_DATE, INTERVAL 6 MONTH);"), params)
+                conn.commit()
 
-        conn.commit()
+        else:
+            conn.commit()
+            return "Already Registered"
+
+
+def delete_register_from_db(data):
+    with engine.connect() as conn:
+        params = {
+            'ID': data['ID#'],
+            'plate': data['Plate#'],
+            'state': data['State']
+        }
+        result_Register = conn.execute(
+            text("select * from Registration where PlateNum = :plate and PlateState = :state"), params)
+        findings = result_Register.all()
+        if len(findings) == 0:
+            conn.commit()
+            return "Person Not Registered"
+        else:
+            conn.execute(text("delete from Registration where IDnum = :ID"), params)
+            conn.commit()
+
+
+def extend_register_from_db(data):
+    with engine.connect() as conn:
+        params = {
+            'ID': data['ID#'],
+            'plate': data['Plate#'],
+            'state': data['State'],
+            'extendDate': data['ExtendDate']
+        }
+        result_Register = conn.execute(text("select * from Registration where PlateNum = :plate and PlateState = :state"), params)
+        findings = result_Register.all()
+        if len(findings) == 0:
+            conn.commit()
+            return "Person Not Registered"
+        else:
+            conn.execute(text("update Registration set EndReg = :extendDate where IDnum = :ID"), params)
+            conn.commit()
+
