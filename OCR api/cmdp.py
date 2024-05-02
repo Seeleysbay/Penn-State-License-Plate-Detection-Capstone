198f83587f01f2e9777f70a2b6d3dfa5cdb1cb4b
bmg5861 <129546481+bmg5861@users.noreply.github.com>
2024-05-01 21:09:32 -0400

    Delete OCR api/cmdp.py

--- OCR api/cmdp.py
+++
@@ -1,8 +0,0 @@
-import subprocess as sp
-
-def check(data: str):
-    results = sp.run(["java", "StandardClassification", data], shell=True, capture_output=True, text=True)
-
-    # results = sp.run([data], shell=True, capture_output=True, text=True)
-    ret: str = results.stdout
-    return ret
